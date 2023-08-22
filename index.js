224728c9e974e003388238e5ace565663dd08f0d
Mauricio <maugaspary@gmail.com>
2023-08-22 14:09:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+//exemplos de variáveis, declaração e paragrafos(p1 e p2)
+
+var nome = "Maurício";
+let idade = 10;
+var aluno = true;
+idade = idade + 1;
+const PI = 3.14159; --> Maiúsculo em constantes é apenas por boa prática
+
+console.log("Olá, ",nome);
+console.log("Você tem ",idade, "anos de idade");
+console.log(aluno);
+
+document.getElementById("p1").innerHTML = nome;
+document.getElementById("p2").innerHTML = "Idade: " + idade;
+*/
+
+/*
+//exemplos de user input
+
+var username = window.prompt("Qual é seu nome?");
+console.log(username);
+console.log(typeof username); --> String
+
+document.getElementById("b1").onclick = function(){
+
+    username = document.getElementById("t1").value;
+    console.log(username);
+    document.getElementById("l1").innerHTML = "Olá, " + username;
+}
+*/
+
+/*
+
+
+
+
+
+
+*/
